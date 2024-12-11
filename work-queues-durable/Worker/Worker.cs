3ede7b1d5b407765b8eed32f282ea30da049cbb2
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-12-11 22:41:06 +0000

    Add examples for durable server queues

---
+++ work-queues-durable/Worker/Worker.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false, autoDelete: false,
+    arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($" [x] Received {message}");
+
+    var dots = message.Split('.').Length - 1;
+    await Task.Delay(dots * 5000);
+
+    Console.WriteLine(" [x] Done");
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("task_queue", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
